91b34f424537cde96a0f901683fd74300f208eeb
mariano-shem <shemmariano56@gmail.com>
2025-08-02 19:23:04 +0800

    sync loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,54 +13,47 @@
   import Intro from "./Intro.svelte";
   import WorkExperience from "./WorkExperience.svelte";
   import TechStackGrid from "./TechStackGrid.svelte";
-
-  // let TechStack = $state(null);
-
-  // $effect(async () => {
-  //   const module = await import("./TechStackGrid.svelte");
-  //   TechStack = module.default;
-  // });
-
+  import Grid from "./Grid.svelte";
   const onGridClick = (url) => goto(url);
-  console.log(data);
 </script>
 
 <div class="grid grid-cols-4 gap-4 grid-rows-5 h-[600px]">
-  <GridCard gridPos="col-span-3 row-span-2"><Hero /></GridCard>
-  <GridCard gridPos="col-span-1 row-span-3">
-    {#if data.error}
-      <div class=" h-full flex items-center justify-center flex-1">
-        <Spinner color="cyan" />
-        <P class="ml-2">Loading data...</P>
-      </div>
-    {:else}
-      <WorkExperience WORK_EXP={data.work_exp} />
-    {/if}
+  <GridCard gridPos="col-span-3 row-span-2"
+    ><Grid {data}>
+      <Hero />
+    </Grid>
   </GridCard>
-  <GridCard gridPos="col-span-3 row-start-3"><Intro /></GridCard>
+  <GridCard gridPos="col-span-1 row-span-3">
+    <Grid {data}>
+      <WorkExperience WORK_EXP={data.work_exp} />
+    </Grid>
+  </GridCard>
+  <GridCard gridPos="col-span-3 row-start-3">
+    <Grid {data}>
+      <Intro />
+    </Grid>
+  </GridCard>
   <GridCard gridPos="flex flex-col col-span-2 row-span-2">
     <Heading tag="h5">Technologies and Tools</Heading>
-    {#if data.error}
-      <div class="flex items-center justify-center flex-1">
-        <Spinner color="cyan" />
-        <P class="ml-2">Loading data...</P>
-      </div>
-    {:else}
+    <Grid {data}>
       <TechStackGrid TECH_STACK={data.tech_stack} />
-    {/if}
+    </Grid>
   </GridCard>
   <GridCard gridPos="cursor-pointer" onclick={() => onGridClick("/projects")}>
-    <div class="container h-full flex flex-col">
-      <Heading class="flex items-center" tag="h5"
-        >Designs<ArrowUpRightFromSquareOutline class="ms-2" size="md" />
-      </Heading>
-      <div class="h-1 w-full flex-1 mt-1 flex gap-1">
-        <div class="rounded-sm bg-cyan-500 flex-1 h-full"></div>
-        <div class="rounded-sm bg-fuchsia-500 flex-1 h-full"></div>
-        <div class="rounded-sm bg-yellow-500 flex-1 h-full"></div>
-        <div class="rounded-sm bg-zinc-500 flex-1 h-full"></div>
+    <Grid {data}>
+      <div class="container h-full flex flex-col">
+        <Heading class="flex items-center" tag="h5">
+          Designs
+          <ArrowUpRightFromSquareOutline class="ms-2" size="md" />
+        </Heading>
+        <div class="h-1 w-full flex-1 mt-1 flex gap-1">
+          <div class="rounded-sm bg-cyan-500 flex-1 h-full"></div>
+          <div class="rounded-sm bg-fuchsia-500 flex-1 h-full"></div>
+          <div class="rounded-sm bg-yellow-500 flex-1 h-full"></div>
+          <div class="rounded-sm bg-zinc-500 flex-1 h-full"></div>
+        </div>
       </div>
-    </div>
+    </Grid>
   </GridCard>
   <GridCard gridPos="row-span-2">5</GridCard>
   <GridCard gridPos="">6</GridCard>
---
+++ src/routes/Grid.svelte
@@ -0,0 +1,13 @@
+<script>
+  let { data, children } = $props();
+  import { Spinner, P } from "flowbite-svelte";
+</script>
+
+{#if data.error}
+  <div class=" h-full flex items-center justify-center flex-1">
+    <Spinner color="cyan" />
+    <P class="ml-2">Loading data...</P>
+  </div>
+{:else}
+  {@render children()}
+{/if}
