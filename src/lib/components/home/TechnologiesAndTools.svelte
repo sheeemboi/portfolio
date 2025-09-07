35820bab91169a2c70c93561135e0575425443ec
Shem Mariano <90581775+sheeemboi@users.noreply.github.com>
2025-09-07 22:54:48 +0800

    fixed padding issue

--- src/lib/components/home/TechnologiesAndTools.svelte
+++ src/lib/components/home/TechnologiesAndTools.svelte
@@ -7,7 +7,7 @@
 <section class="flex flex-col h-full">
   <Heading tag="h5" class="text-center">Current Technologies</Heading>
 
-  <div class="flex flex-wrap gap-3 justify-center w-full my-auto overflow-auto">
+  <div class="flex flex-wrap gap-3 justify-center w-full my-auto p-1 overflow-auto">
     {#each tools as tool}
       <div
         class="bg-zinc-800 flex flex-1 sm:flex-none items-center justify-center rounded-lg p-3 hover:bg-zinc-900 hover:outline hover:outline-1"
