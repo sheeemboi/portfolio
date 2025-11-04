2252704f086a5b828d03905ce6e9df3c5cc02b92
Shem <shemmariano56@gmail.com>
2025-11-04 21:37:56 +0800

    installed vercel/analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import '../app.css';
+  import { injectAnalytics } from '@vercel/analytics/sveltekit';
   import Navbar from '$lib/components/Navbar.svelte';
   import Footer from '$lib/components/Footer.svelte';
+
+  injectAnalytics();
 </script>
 
 <svelte:head>
